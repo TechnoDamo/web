13c616d99368764c2821f04620ec91c2e813d758
Damir Koblev <101816707+TechnoDamo@users.noreply.github.com>
2023-10-25 17:55:58 +0300

    1

--- HW5/calc.js
+++ HW5/calc.js
@@ -1,1 +1,6 @@
-
+function calculate() {
+    const quantity = parseFloat(document.getElementById("quantity").value);
+    const product = parseFloat(document.getElementById("product").value);
+    const totalCost = quantity * product;
+    document.getElementById("result").innerHTML = `Стоимость заказа: ${totalCost}`;
+  }
