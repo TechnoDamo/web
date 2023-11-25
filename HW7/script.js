04416a23b9f192fc0f836f540e12f2230108ee7c
Damir Koblev <101816707+TechnoDamo@users.noreply.github.com>
2023-11-25 16:45:56 +0300

    Add files via upload

---
+++ HW7/script.js
@@ -0,0 +1,26 @@
+/*global $ */
+$(document).ready(function(){
+    $(".slider").slick({
+        arrows: true,
+        dots: true, //точки
+        infinity: true,
+        nextArrow: "<img src='slarR.svg' class='next' alt='2'>",
+        prevArrow: "<img src='slarL.svg' class='prev' alt='1'>",
+        slidesToScroll: 1,
+        slidesToShow: 3,
+        responsive:[
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
